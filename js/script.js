3098f3cbed6aaab28938feb651f6a0e76a8141e1
Brendan Wilson <flubbid@Brendans-MacBook-Pro.local>
2019-05-02 21:18:50 -0500

    Added new files, and added the random function

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+
+// State Variables----------------------
+
+const turns = 3;
+
+// Event Listeners---------------------
+
+// Functions----------------------
+
+
+
+
+//This function will generate the random number for the dice rolls.
+
+
+function getRandom() {
+    return Math.floor(Math.random() * (6 - 1)) + 1;
+}
+
+function roll(dice) {
+    var rand = getRandom();
+}
