878c78cdf57ac035c21da9893c2b81a65dd0d33b
Brendan Wilson <flubbid@Brendans-MacBook-Pro.local>
2019-05-03 17:13:55 -0500

    added switch statements, have the dice working.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@
 
 
 // State Variables----------------------
-
-const turns = 3;
-
+// const turns = 3;
 // Event Listeners---------------------
 
 // Functions----------------------
@@ -13,12 +11,77 @@ const turns = 3;
 
 
 //This function will generate the random number for the dice rolls.
-
-
-function getRandom() {
+class Game{
+constructor(){
+    this.one = "one"
+    this.two = "two"
+    this.three = "three"
+    this.four = "attack"
+    this.five = "energy"
+    this.six = "health"
+}
+getRandom() {
     return Math.floor(Math.random() * (6 - 1)) + 1;
 }
-
-function roll(dice) {
-    var rand = getRandom();
+roll(dice) {
+    let rand = this.getRandom();
+    switch(rand){
+        case 1:
+        this.one
+        break;
+        case 2:
+        this.two
+        break;
+        case 3:
+        this.three
+        break;
+        case 4:
+        this.four
+        break;
+        case 5:
+        this.five
+        break;
+        case 6:
+        this.six
+    }
+    return rand
+    }
 }
+var newGame = new Game();
+
+
+// class User{
+//     constructor(){
+//         this.health = 10;
+//         this.energy = 0;
+//         this.vp = 0;
+//     }
+// handleDamage(attackHeal, points) {
+//  if (attackHeal === 'increase')
+//  this.health += points
+// } else if (attackHeal === 'decrease'){
+// this.health -= points
+// }
+// }
+
+// const player1 = new User()
+// const player2 = new User()
+// const player3 = new User()
+// const player4 = new User()
+// Need function to handle rolling the dice if the class is not 'keep' 
+
+
+
+
+
+
+//This function will act to turn the held action on our off.
+
+
+// $('.die').click(function() {
+//  if ($(this).hasClass('keep')){
+//      $(this).removeClass('keep')
+//     } else {
+//         $(this).addClass('keep')
+//     }
+// })
