9a53f272f4ea6be85f32a348d1b9011a2e880dc6
flubbid <brendanwilson1@gmail.com>
2019-05-04 12:08:25 -0500

    added end turn, change turn, dice roll

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 // const turns = 3;
 // Event Listeners---------------------
 
+
 // Functions----------------------
 
 
@@ -13,14 +14,63 @@
 // potatoes
 //This function will generate the random number for the dice rolls.
 class Game{
-constructor(){
-    this.health = 10
-    this.vp = 0
-    this.energy = 0
-    this.loc = 0
+    constructor(){
+        this.counter = 0
+        this.health = 10
+        this.vp = 0
+        this.energy = 0
+        this.loc = 0
+        this.activePlayer = 0
+        document.querySelector('#endTurn').addEventListener('click', () => {
+            var newPlayer = this.activePlayer + 1;
+            if (newPlayer === 5){
+                newPlayer = 1
+            }
+            this.playerTurn(newPlayer);
+        })
+        document.querySelector('#roll').addEventListener('click',() => {
+            this.counter ++;
+            if(this.counter > 3){
+                alert("Next player turn")
+                return
+            }
+
+            var die1 = this.getRandom();
+            var die2 = this.getRandom();
+            var die3 = this.getRandom();
+            var die4 = this.getRandom();
+            var die5 = this.getRandom();
+            var die6 = this.getRandom();
+            if (document.querySelector("#die1:not(.keep) > .dice") !== null){
+                document.querySelector("#die1:not(.keep) > .dice").innerHTML = die1;
+            }
+            if (document.querySelector("#die2:not(.keep) > .dice") !== null){
+                document.querySelector("#die2:not(.keep) > .dice").innerHTML = die2;
+            }
+            if (document.querySelector("#die3:not(.keep) > .dice") !== null){
+                document.querySelector("#die3:not(.keep) > .dice").innerHTML = die3;
+            }
+            if (document.querySelector("#die4:not(.keep) > .dice") !== null){
+                document.querySelector("#die4:not(.keep) > .dice").innerHTML = die4;
+            }
+            if (document.querySelector("#die5:not(.keep) > .dice") !== null){
+                document.querySelector("#die5:not(.keep) > .dice").innerHTML = die5;
+            }
+            if (document.querySelector("#die6:not(.keep) > .dice") !== null){
+                document.querySelector("#die6:not(.keep) > .dice").innerHTML = die6;
+            }
+        })
 }
 getRandom() {
-    return Math.floor(Math.random() * (7 - 1)) + 1;
+    var randomRoll = Math.floor(Math.random() * (7 - 1)) + 1;
+    if (randomRoll === 4){
+        randomRoll = "A"
+    } else if (randomRoll === 5){
+        randomRoll = "E"
+    } else if (randomRoll === 6){
+        randomRoll = "H"
+    }
+        return randomRoll
 }
 roll() {
     let rand = this.getRandom();
@@ -72,6 +122,17 @@ handleVP3 (){
     //add vp points, it takes 3 of the same dice to give the value on the dice
     console.log("You need 2 more of these to earn VP")
     } 
+playerTurn (id){
+    var allPlayers = document.querySelectorAll('.player')
+    for(var i = 0; i < allPlayers.length; i++){
+        allPlayers[i].style.background ="none";
+    }
+    this.activePlayer = id;
+    document.getElementById(id).style.background ="yellow";
+    this.counter = 0;
+    $('.dieHold').removeClass('keep')
+    $('.dieHold:not(.keep) > .dice').text("0");
+}
 }
 var newGame = new Game();
 
@@ -104,10 +165,10 @@ var newGame = new Game();
 //This function will act to turn the held action on our off.
 
 
-// $('.die').click(function() {
-//  if ($(this).hasClass('keep')){
-//      $(this).removeClass('keep')
-//     } else {
-//         $(this).addClass('keep')
-//     }
-// })
+$('.dieHold').click(function() {
+ if ($(this).hasClass('keep')){
+     $(this).removeClass('keep')
+    } else {
+        $(this).addClass('keep')
+    }
+})
