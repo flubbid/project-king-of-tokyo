35b2c6bd488ad99e6ce3322bd546c57f4e871c5d
flubbid <brendanwilson1@gmail.com>
2019-05-05 12:29:37 -0500

    added to show user active data, added location of users, users now track h/e/vp

--- js/script.js
+++ js/script.js
@@ -20,9 +20,9 @@ class Game{
         this.vp = 0
         this.energy = 0
         this.loc = 0
-        this.activePlayer = 0
+        this.activeUser = 1
         document.querySelector('#endTurn').addEventListener('click', () => {
-            var newPlayer = this.activePlayer + 1;
+            var newPlayer = this.activeUser + 1;
             if (newPlayer === 5){
                 newPlayer = 1
             }
@@ -31,7 +31,7 @@ class Game{
         document.querySelector('#roll').addEventListener('click',() => {
             this.counter ++;
             if(this.counter > 3){
-                alert("Next player turn")
+                alert("It is the next players turn. Please buy or use your cards, and end your turn!")
                 return
             }
 
@@ -98,68 +98,254 @@ roll() {
   handleAttack(){
     // deals damage to players in a seperate location
     // if (location !== userLocation){}
-    console.log("u attack")
-  }
+    var locTokyo = []
+    var locOutsideTokyo = []
+    if (user1.loc === 1){
+        locTokyo.push(user1)
+    } else {
+        locOutsideTokyo.push(user1)
+    }
+    if (user1.loc === 2){
+        locTokyo.push(user2)
+    } else {
+        locOutsideTokyo.push(user2)
+    }
+    if (user1.loc === 3){
+        locTokyo.push(user3)
+    } else {
+        locOutsideTokyo.push(user3)
+    }
+    if (user1.loc === 4){
+        locTokyo.push(user4)
+    } else {
+        locOutsideTokyo.push(user4)
+    }
+    // this push's the users into the outside tokyo and removes health
+    if(this.activeUser === 1){
+        if(user1.loc === 1){
+         locOutsideTokyo.forEach(user => {
+             user.health--;
+         })
+        } else {
+            locTokyo.forEach(user => {
+                user.health--;
+            })
+        }
+    }
+    if(this.activeUser === 2){
+        if(user2.loc === 1){
+         locOutsideTokyo.forEach(user => {
+             user.health--;
+         })
+        } else {
+            locTokyo.forEach(user => {
+                user.health--;
+            })
+        }
+    }
+    if(this.activeUser === 3){
+        if(user3.loc === 1){
+         locOutsideTokyo.forEach(user => {
+             user.health--;
+         })
+        } else {
+            locTokyo.forEach(user => {
+                user.health--;
+            })
+        }
+    }
+    if(this.activeUser === 4){
+        if(user4.loc === 1){
+         locOutsideTokyo.forEach(user => {
+             user.health--;
+         })
+        } else {
+            locTokyo.forEach(user => {
+                user.health--;
+            })
+        }
+    }
+    }
+
   addEnergy(){
 // adds energy to the player who is rolling
-    this.energy++
+    if(this.activeUser === 1){
+        user1.energy++;
+    } else if (this.activeUser === 2){
+        user2.energy++;
+    } else if (this.activeUser === 3){
+        user3.energy++;
+    } else if (this.activeUser === 4){
+        user4.energy++;
+    }
+
   }
  addHealth (){
 //adds health to the player who is rolling if the are not in tokyo(loc1)
-if (this.loc !== 1){
-    this.health++ 
+if (this.activeUser === 1 && user1.loc !== 1 && user1.health < 10){
+    user1.health++;
+} else if (this.activeUser === 2 && user2.loc !== 1 && user2.health < 10) {
+    user2.health++;
+} else if (this.activeUser === 3 && user3.loc !== 1 && user3.health < 10)  {
+    user3.health++;
+} else if (this.activeUser === 4 && user4.loc !== 1 && user4.health < 10) {
+    user4.health++;
 }
+}
+
+//this adds the victory points to the user's
+addVpPoint(points){
+ if(this.activeUser === 1){
+     user1.vP += points
+ } else if (this.activeUser === 2){
+     user2.vP += points
+ } else if (this.activeUser === 3) {
+     user3.vP += points
+ } else if (this.activeUser === 4) {
+     user4.vP += points
  }
- handleVP1 (){
-     //add vp points, it takes 3 of the same dice to give the value on the dice
-    console.log("You need 2 more of these to earn VP")
 }
-handleVP2 (){
-    //add vp points, it takes 3 of the same dice to give the value on the dice
-    console.log("You need 2 more of these to earn VP")  
-}
-handleVP3 (){
-    //add vp points, it takes 3 of the same dice to give the value on the dice
-    console.log("You need 2 more of these to earn VP")
-    } 
+
+//this will show the users info 
+showUserData(){
+    $("#1 > .health").text(user1.health)
+    $("#1 > .vp").text(user1.vP)
+    $("#1 > .energy").text(user1.energy)
+    $("#1 > .loc").text((user1.loc === 1) ? "IN TOKYO" : "OUTSIDE TOKYO") 
+    $("#2 > .health").text(user2.health)
+    $("#2 > .vp").text(user2.vP)
+    $("#2 > .energy").text(user2.energy)
+    $("#2 > .loc").text((user2.loc === 1) ? "IN TOKYO" : "OUTSIDE TOKYO")
+    $("#3 > .health").text(user3.health)
+    $("#3 > .vp").text(user3.vP)
+    $("#3 > .energy").text(user3.energy)
+    $("#3 > .loc").text((user3.loc === 1) ? "IN TOKYO" : "OUTSIDE TOKYO")
+    $("#4 > .health").text(user4.health)
+    $("#4 > .vp").text(user4.vP)
+    $("#4 > .energy").text(user4.energy)
+    $("#4 > .loc").text((user4.loc === 1) ? "IN TOKYO" : "OUTSIDE TOKYO")
+
+} 
+
 playerTurn (id){
+   
+    //this takes the dice output and turns it into an array.
+    var dices = [];
+    $('.dice').each((index, value) => {
+        dices.push(value.innerHTML);
+    })
+
+    
+    var handleVP1 = dices.filter(element => element === "1")
+    if(handleVP1.length >= 3){
+        this.addVpPoint(1) 
+         var extraPoint = handleVP1.length - 3
+         this.addVpPoint(extraPoint)
+    }
+    var handleVP2 = dices.filter(element => element === "2")
+    if(handleVP2.length >= 3){
+        this.addVpPoint(2) 
+         var extraPoint = handleVP2.length - 3
+         this.addVpPoint(extraPoint)
+    }
+    var handleVP3 = dices.filter(element => element === "3")
+    if(handleVP3.length >= 3){
+        this.addVpPoint(3) 
+         var extraPoint = handleVP3.length - 3
+         this.addVpPoint(extraPoint)
+    }
+  console.log("vp", user1.vP, user2.vP, user3.vP, user4.vP)
+
+// this checks the dives for the elements 
+    dices.filter(element => element === "E").forEach(e => {
+        this.addEnergy()
+    })
+    console.log("energy", user1.energy, user2.energy, user3.energy, user4.energy)
+    dices.filter(element => element === "H").forEach(e => {
+        this.addHealth()
+    })
+    
+    
+    //this is used to check who is inside TOKYO. 
+    var userInTokyo; 
+    if(user1.loc === 1){
+        userInTokyo = user1
+    } else if (user2.loc === 1) {
+        userInTokyo = user2
+    } else if (user3.loc === 1) {
+        userInTokyo = user3
+    } else if (user4.loc === 1) {
+        userInTokyo = user4
+    } 
+    var userInTokyoHealth = userInTokyo.health;
+
+    //this is used to handle giving the damage
+    dices.filter(element => element === "A").forEach(e => {
+        this.handleAttack()
+    })
+    console.log("health", user1.health, user2.health, user3.health, user4.health)
+    
+    //this is checking the health of the player inside of tokyo, to see if he lose health and add ap promp to leave and then switchs the location to tokyo of the user who does the damage 
+
+    if(userInTokyoHealth > userInTokyo.health){
+        if (confirm(`Player ${userInTokyo.id} you have taken ${userInTokyoHealth - userInTokyo.health} damage! Do you want to leave tokyo?`)){
+            userInTokyo.loc = 2
+            switch(this.activeUser){
+                case 1:{
+                user1.loc = 1
+                break;
+                }
+                case 2:{
+                user2.loc = 1
+                break;
+                }
+                case 3:{
+                user3.loc = 1
+                break;
+                }
+                case 4:{
+                user4.loc = 1
+                break;
+                }
+            }
+        }
+    }
+    console.log("loc", user1.loc, user2.loc, user3.loc, user4.loc)
+
+    //If I have more than 3 in the array then it needs to 
     var allPlayers = document.querySelectorAll('.player')
     for(var i = 0; i < allPlayers.length; i++){
         allPlayers[i].style.background ="none";
     }
-    this.activePlayer = id;
+    this.activeUser = id;
     document.getElementById(id).style.background ="yellow";
     this.counter = 0;
     $('.dieHold').removeClass('keep')
     $('.dieHold:not(.keep) > .dice').text("0");
+
+    this.showUserData();
 }
 }
 var newGame = new Game();
 
+class User {
+    constructor (id){
+        this.health = 5
+        this.energy = 0
+        this.vP = 0
+        this.id = id
 
-// class User{
-//     constructor(){
-//         this.health = 10;
-//         this.energy = 0;
-//         this.vp = 0;
-//     }
-// handleDamage(attackHeal, points) {
-//  if (attackHeal === 'increase')
-//  this.health += points
-// } else if (attackHeal === 'decrease'){
-// this.health -= points
-// }
-// }
+        if(id === 1){
+            this.loc = 1
+        }
+        else {this.loc = 2}
+    }
+}
 
-// const player1 = new User()
-// const player2 = new User()
-// const player3 = new User()
-// const player4 = new User()
-// Need function to handle rolling the dice if the class is not 'keep' 
-
-
-
-
+var user1 = new User(1)
+var user2 = new User(2)
+var user3 = new User(3)
+var user4 = new User(4)
 
 
 //This function will act to turn the held action on our off.
